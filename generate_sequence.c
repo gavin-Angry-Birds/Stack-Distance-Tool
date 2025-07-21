474bc71b20f620e438f02d6b4820ec872e1e8350
Gavin <332163573@qq.com>
2025-07-21 15:23:40 +0800

    Update generate_sequence.c

--- generate_sequence.c
+++ generate_sequence.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define SEQ_LEN 100
+
+int main() {
+    unsigned int base = 0x1000;
+    unsigned int addr;
+
+    printf("unsigned int addresses[] = {\n");
+    for (int i = 0; i < SEQ_LEN; i++) {
+        if (i % 10 < 7) {
+            addr = base + (rand() % 8) * 4;
+        } else {
+            addr = 0x2000 + (rand() % 50) * 4;
+        }
+        printf("0x%X, ", addr);
+        if ((i + 1) % 8 == 0) printf("\n");
+    }
+    printf("};\n");
+    return 0;
+}
