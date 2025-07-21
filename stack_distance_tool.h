e36c24f83d68cd618a7f8776dbdf78d9ae75578d
Gavin <332163573@qq.com>
2025-07-21 15:24:23 +0800

    Update stack_distance_tool.h

--- stack_distance_tool.h
+++ stack_distance_tool.h
@@ -1,1 +1,12 @@
+#ifndef STACK_DISTANCE_TOOL_H
+#define STACK_DISTANCE_TOOL_H
 
+#define MAX_STACK_SIZE 100
+#define MAX_ACCESSES 1000
+#define HISTOGRAM_BINS 20
+
+void log_access(unsigned int addr);
+void compute_stack_distance_histogram();
+void print_histogram();
+
+#endif // STACK_DISTANCE_TOOL_H
